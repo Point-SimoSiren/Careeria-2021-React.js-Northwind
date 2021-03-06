b8566035a3d4b06cca2d3fc2b3895b7e89000ed7
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-03-06 10:16:07 +0200

    alku commit hakukenttä asiaan

--- src/CustomerAdd.jsx
+++ src/CustomerAdd.jsx
@@ -4,6 +4,7 @@ const CustomerAdd = ({ setLisäystila }) => {
     return (
         <>
             <p>Tähän tulee lisäys formi</p>
+
             <button onClick={() => setLisäystila(false)}>
                 Cancel</button>
         </>
